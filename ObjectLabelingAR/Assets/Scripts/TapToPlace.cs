da3b22d9a593712901ede7db75b07be9ca9e7a40
Gregory Attra <greg.attra@gmail.com>
2021-04-14 14:46:56 -0400

    tap to place

--- ObjectLabelingAR/Assets/Scripts/TapToPlace.cs
+++ ObjectLabelingAR/Assets/Scripts/TapToPlace.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class TapToPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject placementObject;
+    
+    private ARRaycastManager _arRaycastManager;
+    private Pose _placementPose;
+
+    private void Start()
     {
-        
+        _arRaycastManager = FindObjectOfType<ARRaycastManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdatePlacementPose();
+    }
+
+    private void UpdatePlacementPose()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (Input.touchCount == 0)
+            return;
+
+        if (_arRaycastManager.Raycast(
+            Input.GetTouch(0).position,
+            hits, 
+            TrackableType.All))
+        {
+            _placementPose = hits[0].pose;
+            Instantiate(placementObject, _placementPose.position, _placementPose.rotation);
+        }
     }
 }
